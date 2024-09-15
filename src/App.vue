40320b2bd4fc14a7c31fb187ee2cc4db15e75ffa
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-09-14 17:34:04 -0700

    Last minute revisions

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
 import {RouterView} from 'vue-router'
 import NavBar from "@/components/generic/NavBar.vue";
 import BottomFooter from "@/components/generic/BottomFooter.vue";
+import {onMounted} from "vue";
+import {useRoute} from "vue-router";
+import {log_event} from "@/scripts/log_events.js";
+
+let route = useRoute()
+
+function track_leave() {
+  log_event('page_leave', 'nav', `from:${route.name}`)
+}
+
+onMounted(()=>{
+  window.addEventListener('beforeunload', track_leave)
+})
+
 </script>
 
 <template>
