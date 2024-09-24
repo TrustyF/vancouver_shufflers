d26da6bf06069f35b0d49a2dcee4931408de3bf5
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-09-24 15:51:51 -0700

    Added anchor observers back

--- src/App.vue
+++ src/App.vue
@@ -8,34 +8,7 @@ import VideoEmbed from "@/components/embed/VideoEmbed.vue";
 
 let router = useRoute()
 
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      log_event('anchor_in_view', 'scroll', entry.target.id)
-      observer.unobserve(entry.target);
-    }
-  });
-}, {threshold: 0.8});
-
-function track_elements_observer() {
-  const elements = []
-  elements.push(document.getElementById('teacher_anchor'))
-  elements.push(document.getElementById('carousel_anchor'))
-  // elements.push(document.getElementById('schedule_anchor'))
-
-  for (let i in elements) {
-    if (elements[i] === null) {
-      setTimeout(() => track_elements_observer(), 100)
-      return
-    } else {
-      observer.observe(elements[i])
-    }
-  }
-
-}
-
 onMounted(() => {
-  // track_elements_observer()
   ping_user_leave()
   setInterval(() => ping_user_leave(), 5000)
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,38 @@ import TeachersComponent from "@/components/text/TeachersComponent.vue";
 import LinksComponent from "@/components/text/LinksComponent.vue";
 import CarouselComponent from "@/components/generic/CarouselComponent.vue";
 import AboutComponent from "@/components/text/AboutComponent.vue";
+import {log_event} from "@/scripts/log_events.js";
+import {onMounted} from "vue";
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      log_event('anchor_in_view', 'scroll', entry.target.id)
+      observer.unobserve(entry.target);
+    }
+  });
+}, {threshold: 0.8});
+
+function track_elements_observer() {
+  const elements = []
+  elements.push(document.getElementById('teacher_anchor'))
+  elements.push(document.getElementById('carousel_anchor'))
+
+  for (let i in elements) {
+    if (elements[i] === null) {
+      setTimeout(() => track_elements_observer(), 100)
+      return
+    } else {
+      observer.observe(elements[i])
+    }
+  }
+
+}
+
+onMounted(()=>{
+  track_elements_observer()
+})
+
 </script>
 
 <template>
